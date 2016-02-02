e560358f21d0b24f1a55e797decc42ddf7c0c4c2
webiee <email.vandanasethi@gmail.com>
2016-02-02 18:25:13 +0530

    Update search.js

--- www/scripts/search.js
+++ www/scripts/search.js
@@ -14,7 +14,7 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+  
         //app.receivedEvent('deviceready');
         console.log('Cordova device ready event: ' + 'deviceready');
 
@@ -37,7 +37,7 @@ var app = {
          search.startup();
 
       });
-    }
+   
 };
 
 app.initialize();
