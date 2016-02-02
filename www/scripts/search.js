df8f9b57a208dc8dabf4859ea3cad471c239f95b
webiee <email.vandanasethi@gmail.com>
2016-02-02 19:15:22 +0530

    Update search.js

--- www/scripts/search.js
+++ www/scripts/search.js
@@ -1,6 +1,32 @@
-$( document ).ready(function() {
-        alert('hello');
-require([
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+        mapInit();
+    }
+};
+var mapInit = function(){
+	console.log('Cordova device ready event: ' + 'deviceready');
+
+        require([
 
         "esri/map",
         "esri/dijit/Search",
@@ -19,4 +45,7 @@ require([
          search.startup();
 
       });
-});
+    
+
+};
+app.initialize();
