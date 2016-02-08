ca65d03af371af41effae4d05f3936b405d353e7
webiee <email.vandanasethi@gmail.com>
2016-02-08 16:40:04 +0530

    Update lot.js

--- www/scripts/lot.js
+++ www/scripts/lot.js
@@ -1,31 +1,4 @@
-var newapp = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        console.log('Received Event: ' + id);
-        mapInit();
-    }
-};
-var mapInit = function(){
-	console.log('Cordova device ready event: ' + 'deviceready');
-   var map;
+ var map;
     
       require(["esri/map", "esri/layers/ArcGISTiledMapServiceLayer", "dojo/domReady!"],
         function(Map, ArcGISTiledMapServiceLayer) {
@@ -50,8 +23,3 @@ var _baseMapURL2;
           map.addLayer(tiled);
         }
       );
-    
-
-};
-
-newapp.initialize();
